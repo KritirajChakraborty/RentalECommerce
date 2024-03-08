20ade742bf0f0543f107caae0dd1f7d6a8bbb47e
KritirajChakraborty <kritiraj.exams2021@gmail.com>
2024-03-08 23:02:04 +0530

    final fix

--- src/pages/Collections.jsx
+++ src/pages/Collections.jsx
@@ -1,21 +1,31 @@
-import { Link } from "react-router-dom"
-export default function Collections(){
-
-    function handleClick(){
-        window.scrollTo({top: 0, behavior: 'smooth' })
-    }
-    return (
-        <div className='bg-[url("/bgImg.jpg")] bg-cover bg-center bg-no-repeat max-w-screen-2xl mx-auto md:px-28 px-4'>
-            <div className='h-[500px] flex flex-col md:flex-row'>
-                <div className='md:w-1/2'></div>
-                 <div className='md:w-1/2 pt-32'>
-                   <h2 className='font-primary text-3xl font-semibold text-center capitalize p-1 m-1'>Collections</h2>
-                   <p className='font-primary text-center text-Black font-semibold'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Amet quas labore distinctio eius ducimus numquam consectetur sed, cum temporibus, adipisci fugit ipsa! Optio, nostrum reprehenderit.</p>
-                   <Link to='/shop'>
-                   <button onClick={handleClick} className='bg-white text-Black border-none font-semibold px-6 py-2 capitalize rounded-sm hover:bg-orange-500 transition-all duration-200'>See Collections</button>
-                   </Link>
-                </div>
-            </div>
+import { Link } from "react-router-dom";
+export default function Collections() {
+  function handleClick() {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  }
+  return (
+    <div className='bg-[url("/bgImg.jpg")] bg-cover bg-center bg-no-repeat max-w-screen-2xl mx-auto md:px-28 px-4'>
+      <div className="h-[500px] flex flex-col md:flex-row">
+        <div className="md:w-1/2"></div>
+        <div className="md:w-1/2 pt-32 flex flex-col gap-4 items-center">
+          <h2 className="font-primary text-3xl font-semibold text-center capitalize p-1 m-1">
+            Collections
+          </h2>
+          <p className="font-primary text-center text-Black font-semibold">
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Amet quas
+            labore distinctio eius ducimus numquam consectetur sed, cum
+            temporibus, adipisci fugit ipsa! Optio, nostrum reprehenderit.
+          </p>
+          <Link to="/shop">
+            <button
+              onClick={handleClick}
+              className="bg-white text-Black border-none font-semibold px-6 py-2 capitalize rounded-sm hover:bg-orange-500 transition-all duration-200"
+            >
+              See Collections
+            </button>
+          </Link>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,66 +1,97 @@
-import { useState,useEffect } from "react";
+import { useState, useEffect } from "react";
 import Cards from "../components/Cards";
 import { DUMMY_PRODUCTS } from "../dummy-products";
-const initialData = DUMMY_PRODUCTS.slice(0,8);
+const initialData = DUMMY_PRODUCTS.slice(0, 8);
 
-export default function Products(){
-    const [products,setProducts] = useState(initialData);
-    const [filteredItems,setFilteredItems] = useState(initialData);
-    const [sortedOption, setSortedoption] = useState(products);
+export default function Products() {
+  const [products, setProducts] = useState(initialData);
+  const [filteredItems, setFilteredItems] = useState(initialData);
+  const [sortedOption, setSortedoption] = useState(products);
 
-     
-     function handleFilter(category){
-        const filteredItems = category === 'all' ? products : products.filter((item) => item.type === category )
-        //console.log(filteredItems);
-        setFilteredItems(filteredItems)
-     }
+  function handleFilter(category) {
+    const filteredItems =
+      category === "all"
+        ? products
+        : products.filter((item) => item.type === category);
+    //console.log(filteredItems);
+    setFilteredItems(filteredItems);
+  }
 
-     function handleSort(type){
-        const sortedItems = [...filteredItems]
-        console.log(sortedItems);
-        switch (type) {
-            case "A-Z":
-              sortedItems.sort((a, b) => a.title.localeCompare(b.title));
-              break;
-            case "Z-A":
-              sortedItems.sort((a, b) => b.title.localeCompare(a.title));
-              break;
-            case "low-to-high":
-              sortedItems.sort((a, b) => a.price - b.price);
-              break;
-            case "high-to-low":
-              sortedItems.sort((a, b) => b.price - a.price);
-              break;
-            default:
-              break;
-          }
-          setFilteredItems(sortedItems);
-     }
-    return (
-        <div className="max-w-screen-2xl mx-auto md:px-28 px-4 mb-16">
-            <h3 className="font-primary text-3xl font-semibold text-center capitalize p-1 m-1">Suscribe to our Newsletter</h3>
+  function handleSort(type) {
+    const sortedItems = [...filteredItems];
+    console.log(sortedItems);
+    switch (type) {
+      case "A-Z":
+        sortedItems.sort((a, b) => a.title.localeCompare(b.title));
+        break;
+      case "Z-A":
+        sortedItems.sort((a, b) => b.title.localeCompare(a.title));
+        break;
+      case "low-to-high":
+        sortedItems.sort((a, b) => a.price - b.price);
+        break;
+      case "high-to-low":
+        sortedItems.sort((a, b) => b.price - a.price);
+        break;
+      default:
+        break;
+    }
+    setFilteredItems(sortedItems);
+  }
+  return (
+    <div className="max-w-screen-2xl mx-auto md:px-28 px-4 mb-16">
+      <h3 className="font-primary text-3xl font-semibold text-center capitalize p-1 m-1">
+        Suscribe to our Newsletter
+      </h3>
 
-            {/* btns */}
-            <div className="flex flex-col md:flex-row justify-between py-6 lg:px-28 px-4">
-                <div className="flex flex-col md:flex-row flex-wrap justify-start md:gap-8 gap-2 border-none ">
-                    <button className="border-none hover:text-orange-500" onClick={() => handleFilter('all')}>All Products</button>
-                    <button className="border-none  hover:text-orange-500" onClick={() => handleFilter('Furniture')}>Furniture</button>
-                    <button className="border-none  hover:text-orange-500" onClick={() => handleFilter('Electronics')}>Electronics</button>
-                    <button className="border-none  hover:text-orange-500" onClick={() => handleFilter('Utility')}>Utility</button>
-                </div>
-
-                {/* sorting options */}
-                <div className="flex flex-col md:flex-row flex-wrap justify-end gap-6">
-                    <select name="sort" id="sort" multiple={true} onChange={(event) => handleSort(event.target.value)} value={sortedOption} className="bg-black text-white rounded-sm px-1 py-1">
-                        <option value="default" >Default</option>
-                        <option value="A-Z">A to Z</option>
-                        <option value="Z-A">Z to A</option>
-                        <option value="high-to-low">High to Low</option>
-                        <option value="low-to-high">Low to High</option>
-                    </select>
-                </div>
-            </div>
-            <Cards products = {filteredItems}/>
+      {/* btns */}
+      <div className="flex flex-col md:flex-row justify-between py-6 lg:px-28 px-4">
+        <div className="flex flex-col md:flex-row flex-wrap justify-start md:gap-8 gap-2 border-none ">
+          <button
+            className="border-none hover:text-orange-500"
+            onClick={() => handleFilter("all")}
+          >
+            All Products
+          </button>
+          <button
+            className="border-none  hover:text-orange-500"
+            onClick={() => handleFilter("Furniture")}
+          >
+            Furniture
+          </button>
+          <button
+            className="border-none  hover:text-orange-500"
+            onClick={() => handleFilter("Electronics")}
+          >
+            Electronics
+          </button>
+          <button
+            className="border-none  hover:text-orange-500"
+            onClick={() => handleFilter("Utility")}
+          >
+            Utility
+          </button>
         </div>
-    )
+
+        {/* sorting options */}
+        <div className="pt-10 flex flex-col md:flex-row flex-wrap justify-end gap-6">
+          <select
+            name="sort"
+            id="sort"
+            multiple={true}
+            onChange={(event) => handleSort(event.target.value)}
+            value={sortedOption}
+            className="bg-black text-white rounded-sm px-1 py-1"
+          >
+            <option value="default">Default</option>
+            <option value="A-Z">A to Z</option>
+            <option value="Z-A">Z to A</option>
+            <option value="high-to-low">High to Low</option>
+            <option value="low-to-high">Low to High</option>
+          </select>
+        </div>
+      </div>
+      <Cards products={filteredItems} />
+    </div>
+  );
 }
